d80b0dcbf6052d8781e30e212e34c5739c04dd11
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-07-25 01:13:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -40,11 +40,26 @@ class Solution {
         
         for(int i = 0; i < arr.length; i++){
             int count = 0;
-            for(int j = 0; j < arr[i].length; j++){
-                if(arr[i][j] == 1){
-                    count++;
+            int low = 0;
+            int high = arr[i].length - 1;
+            
+            while(low <= high){
+                int mid = (low + high)/2;
+                
+                if(arr[i][mid] == 1){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
                 }
             }
+            
+            count = arr[i].length - low;
+            
+            
+            // for(int j = 0; j < arr[i].length; j++){
+                
+            //         count += arr[i][j];
+            // }
             
             if(count > maxRowNo){
                 maxRowNo = count;
